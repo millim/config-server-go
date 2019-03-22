570de8d07179d941b9d8eaa81718134f30540a95
millim <237092624@qq.com>
2019-03-22 17:51:22 +0800

    [modify] ll

--- routers/configs/createConfig.go
+++ routers/configs/createConfig.go
@@ -1,9 +1,11 @@
 package configs
 
 import (
+	"config-server-go/common/db"
 	"config-server-go/models/config"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,12 +17,10 @@ type CreateConfigFrom struct {
 	Name       string            `json:"name" from:"name"`
 	DataJSON   map[string]string `json:"data_json" from:"data_json"`
 	DataString string            `json:"data_string" from:"data_string"`
-	Cover      bool              `json:"cover" from:"cover"`
 }
 
 func createConfig(c *gin.Context) {
 	var data CreateConfigFrom
-	c.DefaultPostForm("cover", "false")
 	c.ShouldBindJSON(&data)
 
 	config, err := buildConfig(&data)
@@ -29,7 +29,7 @@ func createConfig(c *gin.Context) {
 		return
 	}
 
-	err = saveNewConfig(&config, data.Cover)
+	err = saveNewConfig(&config)
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
 		return
@@ -38,7 +38,13 @@ func createConfig(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"code": 0})
 }
 
-func saveNewConfig(c *config.Config, cover bool) error {
+func saveNewConfig(c *config.Config) error {
+	dbLink := db.GetDB()
+	if dbLink.NewRecord(&c) {
+		dbLink.Create(&c)
+	} else {
+		return errors.New("config name is exists")
+	}
 	return nil
 }
 
@@ -49,15 +55,17 @@ func buildConfig(c *CreateConfigFrom) (config.Config, error) {
 	cf.Env = c.Env
 	cf.Name = c.Name
 
+	fmt.Println(c.DataJSON)
 	if c.DataString != "" {
 		cf.MetaData = []byte(c.DataString)
 		if json.Valid(cf.MetaData) {
 			error = errors.New("data_json format error")
 		}
 	} else if len(c.DataJSON) != 0 {
+		fmt.Println("------dotajson")
 		cf.MetaData, error = json.Marshal(c.DataJSON)
 	} else {
-		error = errors.New("data_json and data_string only one is empty")
+		error = errors.New("data_json and data_string must be a choice")
 	}
 
 	return *cf, error
--- routers/configs/createConfig_test.go
+++ routers/configs/createConfig_test.go
@@ -13,7 +13,7 @@ import (
 
 func Test_createConfigNotData(t *testing.T) {
 	w := httptest.NewRecorder()
-	var jsonStr = []byte(`{"env": "default", "name": "momomo","data_string":"","data_json":{},"cover": false}`)
+	var jsonStr = []byte(`{"env": "default", "name": "momomo","data_string":"","data_json":{}}`)
 	req, _ := http.NewRequest("POST", "/api/configs", bytes.NewReader(jsonStr))
 	testRoute.ServeHTTP(w, req)
 	body, _ := ioutil.ReadAll(w.Body)
@@ -23,8 +23,19 @@ func Test_createConfigNotData(t *testing.T) {
 
 func Test_createConfig(t *testing.T) {
 	w := httptest.NewRecorder()
-	var jsonStr = []byte(`{"env": "default", "name": "momomo","data_string":"","data_json":{"database_name":"test","database_host":"127.0.0.1"},"cover": false}`)
+	var jsonStr = []byte(`{"env": "default", "name": "momomo","data_string":"","data_json":{"database_name":"test","database_host":"127.0.0.1"}}`)
 	req, _ := http.NewRequest("POST", "/api/configs", bytes.NewReader(jsonStr))
+	testRoute.ServeHTTP(w, req)
+	t.Logf("body value ----> %v", w.Body)
+	assert.Equal(t, 200, w.Code)
+}
+
+func Test_notDoubleCreate(t *testing.T) {
+	w := httptest.NewRecorder()
+	var jsonStr = []byte(`{"env": "default", "name": "momomo","data_string":"","data_json":{"database_name":"test","database_host":"127.0.0.1"}}`)
+	req, _ := http.NewRequest("POST", "/api/configs", bytes.NewReader(jsonStr))
+	testRoute.ServeHTTP(w, req)
+	req, _ = http.NewRequest("POST", "/api/configs", bytes.NewReader(jsonStr))
 	testRoute.ServeHTTP(w, req)
 	t.Logf("body value ----> %v", w.Body)
 	assert.Equal(t, 200, w.Code)
